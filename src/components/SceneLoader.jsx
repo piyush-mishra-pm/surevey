c5938626c2114541f180fd8c3828f235d4bd6dec
piyush-mishra-pm <ifs.piyushmishra1991@gmail.com>
2023-10-26 18:20:20 +0530

    temp: Passing to Personal PC

--- src/components/SceneLoader.jsx
+++ src/components/SceneLoader.jsx
@@ -13,11 +13,15 @@ function SceneLoader(props) {
                     <PresentationControls snap global zoom={0.8} rotation={[Math.PI / 4, -Math.PI / 4, 0]} polar={[0, Math.PI / 4]} azimuth={[-Math.PI / 4, Math.PI / 4]}>
                         <ambientLight />
                         <pointLight position={[10, 10, 10]} />
+                        {/** Load Base Model */}
                         <ModelLoader
                             modelPath={props.sceneConfigs.BASE_MODEL_PATH}
                             errorMsg={`${props.sceneConfigs.USER_TYPE} Base Model Path is null.`}
                             successMsg={`${props.sceneConfigs.USER_TYPE} Base Model Loaded!`}
                         />
+
+                        {/** Load Scene Objects */}
+
                     </PresentationControls>
                 </Canvas>
             </div>
--- src/conf/SceneConfigs.js
+++ src/conf/SceneConfigs.js
@@ -1,18 +1,37 @@
 export const SCENE_CONFIGS = {
-    "RESI": {
+    RESI: {
         BASE_MODEL_PATH: "/heavy-assets/models/House.glb",
         USER_TYPE: "RESI",
+        SCENE_OBJECTS: [
+            {
+                APPLIANCE_ID: 'FOX',
+                ASSET_MODEL_PATH: '/heavy-assets/models/Fox.glb',
+                POSITION: { X: 0, Y: 0, Z: 0 },
+                ROTATION: { X: 0, Y: 0, Z: 0 },
+                SCALE: { X: 0, Y: 0, Z: 0 },
+            },
+            {
+                APPLIANCE_ID: 'LANTERN',
+                ASSET_MODEL_PATH: '/heavy-assets/models/Lantern.glb',
+                POSITION: { X: 3, Y: 0, Z: 3 },
+                ROTATION: { X: 0, Y: 0, Z: 0 },
+                SCALE: { X: 0, Y: 0, Z: 0 },
+            },
+        ]
     },
-    "SMB": {
+    SMB: {
         BASE_MODEL_PATH: "/heavy-assets/models/suzanne.gltf",
         USER_TYPE: "SMB",
+        SCENE_OBJECTS: null
     },
-    "LMI": {
+    LMI: {
         BASE_MODEL_PATH: null,
         USER_TYPE: "LMI",
+        SCENE_OBJECTS: null
     },
-    "EV": {
+    EV: {
         BASE_MODEL_PATH: null,
         USER_TYPE: "EV",
+        SCENE_OBJECTS: null
     }
 }
